1fade6f2eaee0d887b75ed9bd52cecce7fff7159
Nar Apresyan <108577622+narekas@users.noreply.github.com>
2022-09-23 13:37:16 +0400

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,3 @@
+# a program that asks the user to enter a number, checks and prints whether the number is even or odd.
+
+print('Even' if int(input('Enter the number. ')) % 2 == 0 else 'Odd')
---
+++ task_4.py
@@ -0,0 +1,3 @@
+# a program that asks the user to enter a number and prints the sum of its digits on the screen.
+
+print(sum(map(int, str(input('Enter the number. ')))))
