71da4bf51726859e54ae8f220c5d9ec9c4b6bdb6
Nar Apresyan <108577622+narekas@users.noreply.github.com>
2022-09-23 13:42:39 +0400

    Delete task_5.py

--- task_5.py
+++
@@ -1,9 +0,0 @@
-# a program that prints the following image on the screen. Use cycles.
-''' *****
-    *   *
-    *   *
-    *   *
-    ***** '''
-
-for i in range(1, 6):
-    print('*' * 5 if i == 1 or i == 5 else '*' + ' ' * 3 + '*')
