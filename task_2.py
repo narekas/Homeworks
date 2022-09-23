e8452c595df3ecf47df550789454424893478ad6
Nar Apresyan <108577622+narekas@users.noreply.github.com>
2022-09-23 13:42:08 +0400

    Delete task_2.py

--- task_2.py
+++
@@ -1,5 +0,0 @@
-# a program that asks the user to enter a character, checks and prints whether the character is a consonant or a vowel.
-
-ch = input('Enter a character: ').lower()
-
-print('is a Vowel' if (ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u') else 'is a Consonant')
