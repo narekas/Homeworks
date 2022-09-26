4bda7307c03e3deb818a9a567a6d4321ca87f41f
Nar Apresyan <108577622+narekas@users.noreply.github.com>
2022-09-26 23:53:53 +0400

    Delete task_2.py

--- hw2/task_2.py
+++
@@ -1,3 +0,0 @@
-# Write a program that asks the user to enter two numbers, x and y , and computes | x − y | /  x+y
-x, y = map(int, input('Enter the number x and y. ').split())
-print(abs(x - y) / x + y)
