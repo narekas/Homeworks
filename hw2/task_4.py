dba381241e8beea93bd269faa79271ba755ee8d2
Nar Apresyan <108577622+narekas@users.noreply.github.com>
2022-09-26 23:54:09 +0400

    Delete task_4.py

--- hw2/task_4.py
+++
@@ -1,9 +0,0 @@
-# Write a program that finds all four of the perfect numbers that are less than 1000.
-
-for i in range(2, 1001):
-    c = 0
-    for j in range(1, int(i // 2) + 1):
-        if i % j == 0:
-            c += j
-    if c == i:
-        print(i)
